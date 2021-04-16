f598c35f7402a08b22686ba17b457679d88cf014
S P Saran Kirthic <sarankirthic@gmail.com>
2021-04-16 22:25:27 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,5 +1,10 @@
 from playsound import playsound
 import music
 
+print(' __ _   _                                   __      ')
+print('  / _) |_) |\\/| /| ._   /\\  |    ._ _      (_ /|  _ ')
+print(" /  _) | \\ |  |  | | | /--\\ | __ | | | |_| __) | (_ ")         
+
 for file in PATH:
 	print(file)
+	
