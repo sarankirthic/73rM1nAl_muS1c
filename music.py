8e25ebaa099d2dca51e6499cfe6fc41196aecab5
S P Saran Kirthic <sarankirthic@gmail.com>
2021-04-16 22:25:25 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,15 +1,24 @@
-import downloader
 import player
 import URL
-
+import os
 # PATh
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 print("")
 print(' __ _   _                                   __      ')
-print('  / _) |_) |\/| /| ._   /\  |    ._ _      (_ /|  _ ')
-print(' /  _) | \ |  |  | | | /--\ | __ | | | |_| __) | (_ ')         
+print('  / _) |_) |\\/| /| ._   /\\  |    ._ _      (_ /|  _ ')
+print(" /  _) | \\ |  |  | | | /--\\ | __ | | | |_| __) | (_ ")         
 
 print('What is on ur Mind?')                                    
 choice = int(input('1. Listen to Music\n2. Download Music\n3. Exit'))
+
+switch
+
+
+
+
+if __name__ == "__main__":
+	
+
+
 
